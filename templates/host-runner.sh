3d4886bc999aac063398a20bbbe27780e2c5dbb3
Jay W <40270505+JayDoubleu@users.noreply.github.com>
2022-01-09 17:52:50 +0000

    Add shims + change flatpak symlinks to files

--- templates/host-runner.sh
+++ templates/host-runner.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 executable=$(basename $0)
 set -x
-exec flatpak-spawn --host $executable "$@"
+exec flatpak-spawn --host --watch-bus --forward-fd=1 --forward-fd=2 --directory=$(pwd) --env=TERM=$TERM $executable "$@"
 
-# Source: https://github.com/containers/toolbox/issues/145#issuecomment-582040463
+# Source: 
+# https://github.com/containers/toolbox/issues/145#issuecomment-582040463
+# https://github.com/containers/toolbox/pull/553/commits/48aa3e507f2c1f7ac742f1bb5ac20e53fbbec45c
