a2976bdc1aabc6b3400085a32cbd12c0c4a9ddde
Jay W <40270505+JayDoubleu@users.noreply.github.com>
2022-01-17 13:49:05 +0000

    More config tweaks

--- templates/host-runner.sh
+++ templates/host-runner.sh
@@ -1,8 +1,5 @@
 #!/bin/bash 
 executable=$(basename $0)
+export PATH=$HOME/.local/bin/:$PATH
 set -x
-exec flatpak-spawn --host --watch-bus --forward-fd=1 --forward-fd=2 --directory=$(pwd) --env=TERM=$TERM $executable "$@"
-
-# Source: 
-# https://github.com/containers/toolbox/issues/145#issuecomment-582040463
-# https://github.com/containers/toolbox/pull/553/commits/48aa3e507f2c1f7ac742f1bb5ac20e53fbbec45c
+exec sockpty $executable $@
